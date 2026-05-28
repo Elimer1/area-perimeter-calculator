a46cccc08266dad12db30a607ae42cecd0d966a7
Eli <elimermz@gmail.com>
2026-05-28 12:29:49 +0300

    added repr

--- calculator.py
+++ calculator.py
@@ -11,6 +11,9 @@ class Shape:
 
     def __str__(self):
         return f"Shape: {self.__class__.__name__} \nArea:{self.get_area()} \nPerimeter: {self.get_perimeter()}"
+    
+    def __repr__(self):
+        return f"{self.__class__.__name__}{tuple(self.__dict__.values())}"
 
     @staticmethod
     def input_validator(num):
@@ -19,3 +22,5 @@ class Shape:
         if num <= 0:
             raise ValueError("Invalid Input: Number must be greater than 0")
         return True
+    
+
--- triangle.py
+++ triangle.py
@@ -1,13 +1,13 @@
-from rectangle import Rectangle
+from calculator import Shape
 
-class Triangle(Rectangle):
+class Triangle(Shape):
     def __init__(self, base, height, side_a, side_b, side_c):
         super().__init__(base, height, side_a, side_b, side_c)
         self.base = base
+        self.height = height
         self.side_a = side_a
         self.side_b= side_b
         self.side_c = side_c
-        print(self.__dict__.values())
 
     def get_area(self):
         return (self.base * self.height) / 2
